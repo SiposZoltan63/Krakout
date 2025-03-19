e721268148cb8d7d6f7f5ab3c836a836dcde7915
SiposZoltan63 <siposz@kkszki.hu>
2025-03-19 09:06:27 +0100

    Pontszám kezelés megy

--- Krakout/MainWindow.xaml.cs
+++ Krakout/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ public partial class MainWindow : Window
 {
         double xSeb = 5;
         double ySeb = 5;
+        int pontszam = 0;
     public MainWindow()
     {
         InitializeComponent();
@@ -31,15 +32,33 @@ public partial class MainWindow : Window
 
     private void mozgatas(object? sender, EventArgs e)
     {
+        //játékos mozgatása
+        Canvas.SetLeft(jatekos, Mouse.GetPosition(jatekter).X);
         var labdaY = Canvas.GetTop(labda);
         var labdaX = Canvas.GetLeft(labda);
         //nézzük a képernyő határait
         if (labdaX > 950) xSeb = -5;
         if (labdaX < 0) xSeb = 5;
-        if (labdaY > 550) ySeb = -5;
+        if (labdaY > 550)
+        { ySeb = -5;
+            //vonjon le egy pontot
+          lbPontszam.Content = --pontszam;
+        }
+
         if (labdaY < 0) ySeb = 5;
-        // a labda mozgatása
-        Canvas.SetLeft(labda, labdaX + xSeb);
+        // ütközési vizsgálat a labda és az ütő között
+        var jatekosX = Canvas.GetLeft(jatekos);
+        var jatekosY = Canvas.GetTop(jatekos);
+        if (labdaX + labda.Width > jatekosX
+            && labdaX < jatekosX + jatekos.Width
+            && labdaY + labda.Height > jatekosY
+            && labdaY < jatekosY + jatekos.Height)
+        {
+            ySeb = -5;
+            lbPontszam.Content = ++pontszam;
+        }
+            // a labda mozgatása
+            Canvas.SetLeft(labda, labdaX + xSeb);
         Canvas.SetTop(labda, labdaY + ySeb);
 
     }
