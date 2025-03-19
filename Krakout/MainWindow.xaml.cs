9e2c0f266ed138a3c7725aa4f82196ed23959281
SiposZoltan63 <siposz@kkszki.hu>
2025-03-19 08:23:36 +0100

    labda pattog

--- Krakout/MainWindow.xaml.cs
+++ Krakout/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Krakout;
 
@@ -16,8 +18,29 @@ namespace Krakout;
 /// </summary>
 public partial class MainWindow : Window
 {
+        double xSeb = 5;
+        double ySeb = 5;
     public MainWindow()
     {
         InitializeComponent();
+        var ido = new DispatcherTimer();
+        ido.Interval = TimeSpan.FromMilliseconds(1);
+        ido.Tick += mozgatas;
+        ido.Start();
+    }
+
+    private void mozgatas(object? sender, EventArgs e)
+    {
+        var labdaY = Canvas.GetTop(labda);
+        var labdaX = Canvas.GetLeft(labda);
+        //nézzük a képernyő határait
+        if (labdaX > 950) xSeb = -5;
+        if (labdaX < 0) xSeb = 5;
+        if (labdaY > 550) ySeb = -5;
+        if (labdaY < 0) ySeb = 5;
+        // a labda mozgatása
+        Canvas.SetLeft(labda, labdaX + xSeb);
+        Canvas.SetTop(labda, labdaY + ySeb);
+
     }
 }
